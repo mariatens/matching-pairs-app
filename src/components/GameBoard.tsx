dead34ec6088ca073f056afc9556e63fc4d1d7d7
mtens17 <mtens17@gmail.com>
2023-02-09 15:12:35 +0000

    Add handleFlipCard function

--- src/components/GameBoard.tsx
+++ src/components/GameBoard.tsx
@@ -1,14 +1,27 @@
+import { useState } from "react";
+import { Card } from "../App";
 import { makeCards } from "../coreGame/game"
 
 export default function GameBoard(): JSX.Element {
-    const allCards = makeCards()
+    const [allCards, setAllCards] = useState<Card[]>(makeCards());
+
+    const handleFlipCard = (cardToFlip:Card) => {
+        const updatedCards: Card[] = allCards.map((oneCard) => {
+            if(cardToFlip.id === oneCard.id){
+                return {...oneCard, life: "faceUp"}
+            }
+        return oneCard
+    })
+        setAllCards(updatedCards)
+    } 
+
     return (
         <>
             <h1>Game Board</h1>
             <div className="all-cards">
                 {allCards.map(card => {
                     return (
-                        <div key={card.id} className = {`card ${card.life}`}>
+                        <div key={card.id} className = {`card ${card.life}`} onClick = {()=> handleFlipCard(card)}>
                             {card.life === "faceUp" && <p>{card.emoji}</p>}
                         </div>
                     )
