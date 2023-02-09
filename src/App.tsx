9fdcaeeec5a94bfd2d22031b2996fa3993f2cbf5
mtens17 <mtens17@gmail.com>
2023-02-09 15:11:58 +0000

    Add interfaces

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
 import GameBoard from "./components/GameBoard";
 import "./styles.css"
+type Emoji = string;
 
+export interface Card{
+  emoji: Emoji;
+  life: "faceUp" | "faceDown" | "removed";
+  id:number;
+}
 function App() {
   return (
     <>
